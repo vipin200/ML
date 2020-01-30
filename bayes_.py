36c66dd5e11a2d09e396349d6bbf41f4df3d62d0
vipin200 <vipin2000kaushik@gmail.com>
2020-01-30 22:57:44 +0530

    probabilities

--- bayes_.py
+++ bayes_.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.utils import shuffle
 import numpy as np
 import random
+import math
 def train_test_split(iris , test_size):
     X = iris.data
     Y = iris.target
@@ -17,7 +18,7 @@ iris = load_iris()
 X_train,X_test,Y_train,Y_test = train_test_split(iris , 0.4)
 
 no_attr = len(X_train[0])     # no of attributes
-un_class = np.unique(Y_train)    # get all unique classes
+un_class ,count_class = np.unique(Y_train , return_counts=True)    # get all unique classes
 no_class = len(un_class)
 ii = np.empty(no_class ,dtype=np.ndarray)
 for i in range(no_class):
@@ -42,4 +43,23 @@ for i in range(no_class):
         mean_xi[i][j] = np.mean(xi[i][j])
         std_xi[i][j] = np.std(xi[i][j])
 
-print(mean_xi)
+# print(mean_xi)
+prob_class = count_class/sum(count_class)
+
+prob_yx = np.empty((len(X_test),no_class))
+
+def prob_xy(X , i, j):  # i : class  -------- j: attribute
+    temp = math.exp(-math.pow(X-mean_xi[i][j],2) / (2 * math.pow(std_xi[i][j],2)))
+    temp1 = (1/math.sqrt(2 * math.pi * std_xi[i][j])) * temp
+    return temp1
+
+for i in range(len(X_test)):
+
+    for j in range(no_class):
+        prob_yx[i][j] = prob_class[j]
+        for k in range(no_attr):
+            prob_yx[i][j] *= prob_xy(X_test[i][k] , j , k)
+
+
+
+print(prob_yx)
